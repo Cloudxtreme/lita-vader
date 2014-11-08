ec52f21d25b919901bca5278ed7b86c47e41fcb4
Brian Pitts <brian@polibyte.com>
2014-11-08 14:43:55 -0600

    Add failing test for route

--- spec/lita/handlers/vader_spec.rb
+++ spec/lita/handlers/vader_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::Vader, lita_handler: true do
+
+  it 'routes questions about fathers' do
+    expect(subject).to route_command("who is Luke's father?").to(:father)
+  end
 end
