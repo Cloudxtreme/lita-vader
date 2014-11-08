284cb484f55f2f5a260b4ffe90b007d5c20d19ce
Brian Pitts <brian@polibyte.com>
2014-11-08 15:17:41 -0600

    Add response method

--- lib/lita/handlers/vader.rb
+++ lib/lita/handlers/vader.rb
@@ -2,6 +2,11 @@ module Lita
   module Handlers
     class Vader < Handler
       route(/who is (.*)'s father\?/, :father)
+
+      def father(response)
+        child = response.matches[0][0]
+        response.reply("@#{child}, I am your father!")
+      end
     end
 
     Lita.register_handler(Vader)
--- spec/lita/handlers/vader_spec.rb
+++ spec/lita/handlers/vader_spec.rb
@@ -6,7 +6,7 @@ describe Lita::Handlers::Vader, lita_handler: true do
     expect(subject).to route_command("who is Luke's father?").to(:father)
   end
 
-  describe :father do
+  describe 'father' do
     it "explains who a user's father is" do
       send_command("who is Luke's father?")
       expect(replies.last).to eq("@Luke, I am your father!")
