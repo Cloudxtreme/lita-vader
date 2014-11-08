8beca4b68072d6c2a9af5a2033fbbfdefa8b083f
Lee Jones <scribblethink@gmail.com>
2014-11-08 14:55:38 -0600

    Add test for response

--- spec/lita/handlers/vader_spec.rb
+++ spec/lita/handlers/vader_spec.rb
@@ -5,4 +5,11 @@ describe Lita::Handlers::Vader, lita_handler: true do
   it 'routes questions about fathers' do
     expect(subject).to route_command("who is Luke's father?").to(:father)
   end
+
+  describe :father do
+    it "explains who a user's father is" do
+      send_command("who is Luke's father?")
+      expect(replies.last).to eq("@Luke, I am your father!")
+    end
+  end
 end
